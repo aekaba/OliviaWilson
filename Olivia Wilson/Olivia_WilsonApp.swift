ff417a8e84310abfd41d7cb6925ad4f6b091b224
Ata Emir Kaba <ataemirkaba@icloud.com>
2022-10-16 13:58:02 +0300

    Initial Commit

---
+++ Olivia Wilson/Olivia_WilsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Olivia_WilsonApp.swift
+//  Olivia Wilson
+//
+//  Created by Ata emir on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Olivia_WilsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
